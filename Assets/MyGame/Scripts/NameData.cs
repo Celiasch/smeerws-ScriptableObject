895f03fb52270d3b13c5cf609e56170609d065f2
Celiasch <clea.eliasch@htl-salzburg.ac.at>
2019-02-06 13:21:42 +0100

    Fixed and name works

---
+++ Assets/MyGame/Scripts/NameData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "NameData")]
+public class NameData : ScriptableObject {
+    public string yourName = "empty"; 
+} //muss direkt darunter stehen, sonst funktioniert das nicht. Keine Leerzeile zwischen CreateAssetsMenu und klasse
